97687758d95ffc4eb47f3a46538fa674f99f328d
Robert F. Dickerson <rfdickerson@gmail.com>
2016-03-10 11:04:36 -0600

    Updated the Kitura version to 0.4

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
   name: "TodoList",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,3,0)..<Version(0,4,0)),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,4,0)..<Version(0,5,0)),
     .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", versions: Version(0,0,0)..<Version(0,1,0))
   ]
 )
