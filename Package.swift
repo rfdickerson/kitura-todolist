3869a518f55d387363bbebd8c9de514d6b97e362
Robert F. Dickerson <rfdickerson@gmail.com>
2016-03-22 18:48:29 -0500

    added the Package file for installing dependencies with apt-get on Buildpacks

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ import PackageDescription
 let package = Package(
   name: "TodoList",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura.git", versions: Version(0,5,0)..<Version(0,6,0)),
-    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", versions: Version(0,4,0)..<Version(0,5,0))
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0, minor: 4),
   ]
 )
