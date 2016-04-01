7c8945615e4ea5a939ac0a4841b06d0956bfd741
Robert F. Dickerson <rfdickerson@gmail.com>
2016-04-01 11:47:03 -0500

    Fixed routes to pass tests
    IBM-Swift/Kitura#337

--- Sources/TodoList/Controllers/App.swift
+++ Sources/TodoList/Controllers/App.swift
@@ -26,12 +26,12 @@ import SwiftyJSON
 ///
 class AllRemoteOriginMiddleware: RouterMiddleware {
     func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
-        
-        Log.info("Added cross origin header")
-        
+
+        // Log.info("Added cross origin header")
+
         response.setHeader("Access-Control-Allow-Origin", value: "*")
         //response.setHeader("Access-Control-Allow-Headers", value: "Origin, X-Requested-With, Content-Type, Accept")
-        
+
         next()
     }
 }
@@ -42,16 +42,16 @@ class AllRemoteOriginMiddleware: RouterMiddleware {
 func setupRoutes(router: Router, todos: TodoCollection) {
 
     router.all("/*", middleware: BodyParser())
-    
+
     router.all("/*", middleware: AllRemoteOriginMiddleware())
 
-    
-    router.all("/") {
-        _, _, next in
-        
-        next()
-    }
-        
+
+    // router.all("/") {
+    //    _, _, next in
+    //
+    //    next()
+    // }
+
     ///
     /// Get all the todos
     ///
@@ -64,21 +64,45 @@ func setupRoutes(router: Router, todos: TodoCollection) {
 
         next()
     }
+    
+    router.get("/todos/:id") {
+        request, response, next in
+
+        let id: String? = request.params["id"]
+        
+        guard id != nil else {
+            response.status(HttpStatusCode.BAD_REQUEST)
+            return
+        }
+        
+        let item = todos.get(id!)
+        
+        if let item = item {
+            
+            let result = JSON(item.serialize())
+            
+            do {
+                try response.status(HttpStatusCode.OK).sendJson(result).end()
+            } catch {}
+            
+        }
+        
+    }
 
     ///
     /// Handle options
     ///
     router.options("/*") {
         request, response, next in
-        
+
         response.setHeader("Access-Control-Allow-Headers", value: "accept, content-type")
         response.setHeader("Access-Control-Allow-Methods", value: "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH")
-        
+
         response.status(HttpStatusCode.OK)
-        
+
         next()
     }
-    
+
     ///
     /// Add a todo list item
     ///
@@ -90,14 +114,20 @@ func setupRoutes(router: Router, todos: TodoCollection) {
             if let json = body.asJson() {
 
                 let title = json["title"].stringValue
-                let order = json["order"].intValue
-                let completed = json["completed"].boolValue
+                let order = json["order"].intValue ?? 0
+                let completed = json["completed"].boolValue ?? false
+
+                Log.info("Received \(title)")
 
                 let newItem = todos.add(title, order: order, completed: completed)
 
                 let result = JSON(newItem.serialize())
+        
+                do  {
+                    try response.status(HttpStatusCode.OK).sendJson(result).end()
+                } catch {}
 
-                response.status(HttpStatusCode.OK).sendJson(result)
+               
 
             }
         } else {
@@ -107,34 +137,34 @@ func setupRoutes(router: Router, todos: TodoCollection) {
 
         // next()
   }
-    
+
     router.post("/todos/:id") {
         request, response, next in
-        
+
         let id: String? = request.params["id"]
-        
+
         if let body = request.body {
-            
+
             if let json = body.asJson() {
-                
+
                 let title = json["title"].stringValue
                 let order = json["order"].intValue
                 let completed = json["completed"].boolValue
-                
+
                 let newItem = todos.update(id!, title: title, order: order, completed: completed)
-                
+
                 let result = JSON(newItem!.serialize())
-                
+
                 response.status(HttpStatusCode.OK).sendJson(result)
-                
+
             }
         } else {
             Log.warning("No body")
             response.status(HttpStatusCode.BAD_REQUEST)
         }
 
-        
-        
+
+
     }
 
   ///
@@ -144,17 +174,17 @@ func setupRoutes(router: Router, todos: TodoCollection) {
     request, response, next in
 
     let id: String? = request.params["id"]
-    
+
     if let id = id {
-        
+
         if let body = request.body {
-            
+
             if let json = body.asJson() {
-                
+
                 let completed = json["completed"].boolValue
-                
+
                 todos.update(id, title: nil, order: nil, completed: completed)
-                
+
                 response.status(HttpStatusCode.OK)
 
             }
@@ -162,53 +192,57 @@ func setupRoutes(router: Router, todos: TodoCollection) {
             response.status(HttpStatusCode.BAD_REQUEST)
         }
     }
-    
+
     next()
 
   }
 
-  
+
 
   ///
   /// Delete an individual todo item
   ///
   router.delete("/todos/:id") {
     request, response, next in
-    
+
     Log.info("Requesting a delete")
-    
+
     let id: String? = request.params["id"]
-    
+
     if let id = id {
         todos.delete(id)
     } else {
         Log.warning("Could not parse ID")
     }
-    
+
     do {
         try response.status(HttpStatusCode.OK).end()
     } catch {
-    
-    
+
+
     }
     // next()
 
   }
-    
+
     ///
     /// Delete all the todo items
     ///
-    router.delete("/todos/") {
+    router.delete("/") {
         request, response, next in
-        
+
         Log.info("Requested clearing the entire list")
-        
+
         todos.clear()
-        
+
+        do {
+            try response.status(HttpStatusCode.OK).end()
+        } catch { }
+
         //next()
-        
+
     }
-    
+
     ///
     ///
     ///
