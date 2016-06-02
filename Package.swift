3775bb2097185733a5aa2e3fcf4f1f1709675650
Daniel Firsht <dfirsht@us.ibm.com>
2016-06-02 10:44:00 -0500

    Updated dependency versions

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     name: "KituraTodoList",
     dependencies: [
                       .Package(url: "https://github.com/IBM-Swift/todolist-couchdb.git", majorVersion: 0),
-                      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 13),
+                      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 15),
                       .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 7),
                       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1)
     ]
