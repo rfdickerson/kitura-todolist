2e243802bc177ef01e195144fb235e78aa93af1c
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-06-09 11:07:34 -0500

    Fixing down cfenv minor version to prevent broken builds

--- Package.swift
+++ Package.swift
@@ -22,6 +22,6 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/todolist-couchdb.git", majorVersion: 0, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 16),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1)
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1, minor: 2)
     ]
 )
