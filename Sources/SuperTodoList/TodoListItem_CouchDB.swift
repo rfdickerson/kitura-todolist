189a8ed662f1adf5dec2ccc352200b77186fa6ab
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-10-05 12:13:00 -0500

    Talks to CouchDB

--- Sources/SuperTodoList/TodoListItem_CouchDB.swift
+++ Sources/SuperTodoList/TodoListItem_CouchDB.swift
@@ -1,11 +1,35 @@
 import Foundation
 import Kitura
 import SwiftyJSON
-
+import MiniPromiseKit
 
 /**
  This example will communicate with a CouchDB database
  */
+
+let queue = DispatchQueue(label: "Something", attributes: .concurrent)
+let hostname = "http://127.0.0.1:5984"
+let database = "todolist"
+let design = "tododesign"
+
+let url = URL(string: "http://127.0.0.1:5984/todolist/_design/tododesign/_view/all_todos")
+
+func getAllItems() -> Promise<[TodoItem]> {
+    return firstly {
+        URLSession().dataTaskPromise(with: url!)
+        }.then(on: queue ) { data -> [TodoItem] in
+            let json = try JSONSerialization.jsonObject(with: data,
+                                                        options: [])
+            let j = json as? [String: Any]
+            let rows = j!["rows"] as? [[String: Any]]
+            
+            return rows!.map { row in
+                let values = row["value"] as? [String]
+                
+                return TodoItem(id: UUID(), title: "stuff")
+            }
+    }
+}
 
 func handleGetCouchDBItems(
     request: RouterRequest,
@@ -13,6 +37,18 @@ func handleGetCouchDBItems(
     callNextHandler: @escaping () -> Void) throws {
     response.send(json: JSON(itemStrings))
     callNextHandler()
+    
+    _ = firstly {
+        getAllItems()
+    }.then(on: queue) { items in
+        print(items)
+    }.catch(on: queue) { error in
+        print(error)
+    }.always(on: queue) {
+            callNextHandler()
+    }
+    
+    
 }
 
 
